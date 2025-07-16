44afd6aa4504f62db68412b239ae59dc468392a9
hey-its-Sohan <its.sohan.dev@gmail.com>
2025-07-17 02:19:22 +0600

    express, cors and port setup

---
+++ index.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Get Ready To Share Bite!')
+})
+
+app.listen(port, () => {
+  console.log(`Share Bite is running on port: ${port}`)
+})
