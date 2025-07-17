3b7c6a85221e36c26a03dfc1b8561a3ea7189bcb
hey-its-Sohan <its.sohan.dev@gmail.com>
2025-07-17 12:01:55 +0600

    connected to mongoDB server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,6 +8,47 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zodydsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const shareBitesCollection = client.db('shareBiteDB').collection('shareBite')
+
+    // get API for all foods data
+    app.get('/all-foods', async (req, res) => {
+      const cursor = shareBitesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
